64da9bcd15f85a683d424afca53e2aaab13db9f6
germantoledo <gefetoledo@gmail.com>
2022-05-26 00:07:59 -0400

    commit 26/05/2022 00:07

---
+++ portafolio/js/main.js
@@ -0,0 +1,12 @@
+$('#facebook').on('click', function() {
+    window.open('https://www.facebook.com/germanfedericotoledocoronado/', '_blank');
+});
+
+$('#linkedin').on('click', function() {
+    window.open('https://www.linkedin.com/in/german-federico-toledo-coronado-655546194', '_blank');
+});
+
+// descargar CV
+$('.botonCV').on('click', function() {
+    window.open('../assets/doc/cv.pdf');
+});
