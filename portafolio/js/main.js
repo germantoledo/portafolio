0374e53a48a0fcf4c825daa5c364257fafd8d759
germantoledo <gefetoledo@gmail.com>
2022-06-01 17:24:26 -0400

    commit 01/06/2022 17:24

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -1,13 +1,15 @@
-$('#facebook').on('click', function() {
+$('#facebook').on('click', function () {
     window.open('https://www.facebook.com/germanfedericotoledocoronado/', '_blank');
 });
 
-$('#linkedin').on('click', function() {
+$('#linkedin').on('click', function () {
     window.open('https://www.linkedin.com/in/german-federico-toledo-coronado-655546194', '_blank');
 });
 
-$('#whatsapp').on('click', function() {
+$('#whatsapp').on('click', function () {
     window.open('https://wa.me/59175359174?text=Hola%20te%20escribo%20desde%20tu%20pagina', '_blank');
 });
 
-
+$('#trabajosBO').on('click', function () {
+    window.open('https://trabajosbo.netlify.app', '_blank');
+});
