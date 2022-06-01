bce2a8ca682599c540f177a8e3f9f240f6eb9f54
germantoledo <gefetoledo@gmail.com>
2022-06-01 14:08:50 -0400

    commit 01/06/2022 14:08

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -5,3 +5,9 @@ $('#facebook').on('click', function() {
 $('#linkedin').on('click', function() {
     window.open('https://www.linkedin.com/in/german-federico-toledo-coronado-655546194', '_blank');
 });
+
+$('#whatsapp').on('click', function() {
+    window.open('https://wa.me/59175359174?text=Hola%20te%20escribo%20desde%20tu%20pagina', '_blank');
+});
+
+
