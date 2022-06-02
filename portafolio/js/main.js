8ce8167a95c2b4e585fa7ba793c34b11b309eaee
germantoledo <gefetoledo@gmail.com>
2022-06-02 16:34:52 -0400

    commit 02/06/2022 16:34

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -14,11 +14,53 @@ $('#trabajosBO').on('click', function () {
     window.open('https://trabajosbo.netlify.app', '_blank');
 });
 
+$('#recetasApp').on('click', function () {
+    window.open('https://recetasapp.netlify.app', '_blank');
+});
+
+$('#zlatan').on('click', function () {
+    window.open('https://www.zlatancorporacion.com', '_blank');
+});
+
+
+$('#clinica').on('click', function () {
+    window.open('https://play.google.com/store/apps/details?id=com.clinicafoianini', '_blank');
+});
+
+
+
 
 $('.imagen').mouseover(function() {
     $('.sobreImagen').css('visibility','visible');
 });
 
 $('.imagen').mouseout(function() {
+    $('.sobreImagen').css('visibility','hidden');
+}); 
+
+
+
+$('.imagen2').mouseover(function() {
+    $('.sobreImagen').css('visibility','visible');
+});
+
+$('.imagen2').mouseout(function() {
+    $('.sobreImagen').css('visibility','hidden');
+}); 
+
+
+$('.imagen3').mouseover(function() {
+    $('.sobreImagen').css('visibility','visible');
+});
+
+$('.imagen3').mouseout(function() {
+    $('.sobreImagen').css('visibility','hidden');
+}); 
+
+$('.imagen4').mouseover(function() {
+    $('.sobreImagen').css('visibility','visible');
+});
+
+$('.imagen4').mouseout(function() {
     $('.sobreImagen').css('visibility','hidden');
 }); 
